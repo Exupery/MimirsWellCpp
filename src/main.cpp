fe02b4e9ced711da1f95ad84c9a7a715c48b571f
Matthew Frost <mf@matthewfrost.com>
2012-08-18 15:58:06 -0400

    twitter search api testing

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,41 @@
 */
 
 #include <iostream>
+#include <string>
 #include <stdio.h>
+#include <curl/curl.h>
+#include <curl/easy.h>
 
-int main(int argc, char *argv[]) {
-	std::cout << "test" << std::endl;
+void apitest();
 
+int main(int argc, char * argv[]) {
+	std::cout << "testing twitter api" << std::endl;
+	apitest();
 	return 0;
+}
+
+int curlWrite(char *data, size_t size, size_t len, std::string * buffer) {
+	int result = 0;
+	if (buffer != NULL) {
+		buffer->append(data, size * len);
+		result = size * len;
+	}
+	return result;
+}
+
+void apitest() {
+	std::cout << "searching twitter for 'frost'" << std::endl;
+	CURL * curl;
+	std::string buffer;
+	std::string testurl = "http://search.twitter.com/search.json?q=frost";
+	const char * url = testurl.c_str();
+	curl = curl_easy_init();
+	if (curl) {
+		curl_easy_setopt(curl, CURLOPT_URL, url);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curlWrite);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
+
+		curl_easy_perform(curl);
+		curl_easy_cleanup(curl);
+	}
 }
