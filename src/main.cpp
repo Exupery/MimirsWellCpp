0ecceb3b2f3ca6e7769d3d0c6cbd7b51949150e2
Matthew Frost <mf@matthewfrost.com>
2012-08-18 12:32:49 -0400

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+/*
++=======================================================+
+|	Name        : Mimir's Well							|
+|	Author      : Matthew Frost							|
+|	Version     : 0.1									|
+|	Copyright   : "BEER-WARE LICENSE"					|
+|	Description : Stock Market Sentiment Analyzer		|
++=======================================================+
+*/
+
+#include <iostream>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	std::cout << "test" << std::endl;
+
+	return 0;
+}
