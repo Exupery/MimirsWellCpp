4e5da7a654c6446a65894e4ae0a49d1f755dc625
Matthew Frost <mf@matthewfrost.com>
2012-08-19 17:57:48 -0400

    jansson testing

--- src/parser.cpp
+++ src/parser.cpp
@@ -5,6 +5,7 @@
  *      Author: frost
  */
 
+#include <iostream> //DELME
 #include <jansson.h>
 #include "parser.h"
 
@@ -16,15 +17,25 @@ Parser::~Parser() {
 
 }
 
-std::set<Tweet> Parser::parseJSON(std::string json) {
+std::set<Tweet> Parser::parseJSON(const std::string json) {
 	std::set<Tweet> tweets;
 	json_error_t error;
-	json_t * root;
-	root = json_loads(json.c_str(), 0, &error);
+	json_t * root, * data, * message;
+	const char * message_text;
+	char * text = (char *)"{\"created_at\":\"Sat, 18 Aug 2012 20:14:17 +0000\",\"from_user_id\":106725205,\"id\":236918965404848128,\"text\":\"testing...\"}";
+//	char * text = (char *)json.c_str();
+	root = json_loads(text, 0, &error);
 	if (root) {
 		Tweet t;
 		tweets.insert(t);
+		data = json_array_get(root, 0);
+		message = json_object_get(data, "created_at");
+		message_text = json_string_value(message);
+		std::cout << message_text << std::endl;
+	} else {
+		std::cout << error.text << std::endl;
 	}
+
 	return tweets;
 }
 
