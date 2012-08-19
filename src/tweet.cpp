89a3998f2c041243d1d8d37638938122d0123ff9
Matthew Frost <mf@matthewfrost.com>
2012-08-19 14:01:57 -0400

    added Tweet getters and setters

--- src/tweet.cpp
+++ src/tweet.cpp
@@ -8,10 +8,45 @@
 #include "tweet.h"
 
 Tweet::Tweet() {
-
+	userID = 0;
+	id = 0;
+	postedAt = 0;
+	text = "";
 }
 
 Tweet::~Tweet() {
 
+}
+
+void Tweet::setUserID(int userID) {
+	this->userID = userID;
+}
+
+void Tweet::setID(long id) {
+	this->id = id;
+}
+
+void Tweet::setPostedAt(long postedAt) {
+	this->postedAt = postedAt;
+}
+
+void Tweet::setText(std::string text) {
+	this->text = text;
+}
+
+int Tweet::getUserID() {
+	return userID;
+}
+
+long Tweet::getID() {
+	return id;
+}
+
+long Tweet::getPostedAt() {
+	return postedAt;
+}
+
+std::string Tweet::getText() {
+	return text;
 }
 
--- src/tweet.h
+++ src/tweet.h
@@ -5,13 +5,28 @@
  *      Author: frost
  */
 
+#include <string>
+
 #ifndef TWEET_H_
 #define TWEET_H_
 
 class Tweet {
+private:
+	int userID;
+	long id;
+	long postedAt;
+	std::string text;
 public:
 	Tweet();
 	virtual ~Tweet();
+	void setUserID(int userID);
+	void setID(long id);
+	void setPostedAt(long postedAt);
+	void setText(std::string text);
+	int getUserID();
+	long getID();
+	long getPostedAt();
+	std::string getText();
 };
 
 #endif /* TWEET_H_ */
