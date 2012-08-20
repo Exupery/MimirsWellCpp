46e51a9ca18bc32e4bce2928635ec995351f738a
Matthew Frost <mf@matthewfrost.com>
2012-08-20 19:44:28 -0400

    parse all fields

--- src/parser.cpp
+++ src/parser.cpp
@@ -21,23 +21,29 @@ std::set<Tweet> Parser::parseJSON(const std::string json) {
 	std::set<Tweet> tweets;
 	json_error_t error;
 	json_t * root;
-	char * text = (char *)"[{\"created_at\":\"Sat, 18 Aug 2012 20:14:17 +0000\",\"from_user_id\":106725205,\"id\":236918965404848128,\"text\":\"testing...\"}]";
-//	char * text = (char *)json.c_str();
-	root = json_loads(text, 0, &error);
+	json_t * data, * createdAt, * userID, * id, * text;
+	char * source = (char *)"[{\"created_at\":\"Sat, 18 Aug 2012 20:14:17 +0000\",\"from_user_id\":106725205,\"id\":236918965404848128,\"text\":\"testing...\"}]";
+//	char * source = (char *)json.c_str();
+	root = json_loads(source, 0, &error);
 	if (root) {
 		Tweet t;
 		tweets.insert(t);
 		if (json_is_array(root)) {
-			for (int i=0; json_array_size(root); i++) {
-				json_t * data, * message;
-				const char * message_text;
+			for (size_t i=0; i < json_array_size(root); i++) {
+//				json_t * data, * createdAt, * userID, * id, * text;
+//				const char * message_text;
 				data = json_array_get(root, i);
 				if (json_is_object(data)) {
-					message = json_object_get(data, "created_at");
-					message_text = json_string_value(message);
-					std::cout << message_text << std::endl;
+					createdAt = json_object_get(data, "created_at");
+					userID = json_object_get(data, "from_user_id");
+					id = json_object_get(data, "id");
+					text = json_object_get(data, "text");
 				}
 			}
+			std::cout << json_string_value(createdAt) << std::endl;
+			std::cout << json_number_value(userID) << std::endl;
+			std::cout << json_number_value(id) << std::endl;
+			std::cout << json_string_value(text) << std::endl;
 		} else {
 			std::cout << "JSON array expected!" << std::endl;
 		}
