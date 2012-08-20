50654a036626ea429f28ec09447784a5cfecc110
Matthew Frost <mf@matthewfrost.com>
2012-08-20 19:25:49 -0400

    parsed created_at

--- src/parser.cpp
+++ src/parser.cpp
@@ -21,7 +21,7 @@ std::set<Tweet> Parser::parseJSON(const std::string json) {
 	std::set<Tweet> tweets;
 	json_error_t error;
 	json_t * root;
-	char * text = (char *)"{\"results\":[{\"created_at\":\"Sat, 18 Aug 2012 20:14:17 +0000\",\"from_user_id\":106725205,\"id\":236918965404848128,\"text\":\"testing...\"}]}";
+	char * text = (char *)"[{\"created_at\":\"Sat, 18 Aug 2012 20:14:17 +0000\",\"from_user_id\":106725205,\"id\":236918965404848128,\"text\":\"testing...\"}]";
 //	char * text = (char *)json.c_str();
 	root = json_loads(text, 0, &error);
 	if (root) {
