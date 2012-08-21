729bf921760bf8e418b79e0c0d5aaf59284c9a33
Matthew Frost <mf@matthewfrost.com>
2012-08-20 20:24:18 -0400

    parsed data added to tweet object

--- src/parser.cpp
+++ src/parser.cpp
@@ -21,29 +21,44 @@ std::set<Tweet> Parser::parseJSON(const std::string json) {
 	std::set<Tweet> tweets;
 	json_error_t error;
 	json_t * root;
-	json_t * data, * createdAt, * userID, * id, * text;
 	char * source = (char *)"[{\"created_at\":\"Sat, 18 Aug 2012 20:14:17 +0000\",\"from_user_id\":106725205,\"id\":236918965404848128,\"text\":\"testing...\"}]";
 //	char * source = (char *)json.c_str();
 	root = json_loads(source, 0, &error);
 	if (root) {
-		Tweet t;
-		tweets.insert(t);
 		if (json_is_array(root)) {
 			for (size_t i=0; i < json_array_size(root); i++) {
-//				json_t * data, * createdAt, * userID, * id, * text;
-//				const char * message_text;
+				json_t * data, * createdAt, * userID, * id, * text;
 				data = json_array_get(root, i);
 				if (json_is_object(data)) {
+					Tweet t;
+
 					createdAt = json_object_get(data, "created_at");
+					if (json_is_string(createdAt)) {
+						t.setPostedAt(0L);
+					}
+
 					userID = json_object_get(data, "from_user_id");
+					if (json_is_number(userID)) {
+						t.setUserID((long)json_number_value(userID));
+					}
+
 					id = json_object_get(data, "id");
+					if (json_is_number(id)) {
+						t.setID((long)json_number_value(id));
+					}
+
 					text = json_object_get(data, "text");
+					if (json_is_string(text)) {
+						t.setText(json_string_value(text));
+					}
+
+					tweets.insert(t);
 				}
 			}
-			std::cout << json_string_value(createdAt) << std::endl;
-			std::cout << json_number_value(userID) << std::endl;
-			std::cout << json_number_value(id) << std::endl;
-			std::cout << json_string_value(text) << std::endl;
+//			std::cout << t.getPostedAt() << std::endl;
+//			std::cout << t.getUserID() << std::endl;
+//			std::cout << t.getID() << std::endl;
+//			std::cout << t.getText() << std::endl;
 		} else {
 			std::cout << "JSON array expected!" << std::endl;
 		}
