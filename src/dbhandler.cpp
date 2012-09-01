5c08ce839ec0a7d327614ad0f1564f3cba001c04
Matthew Frost <mf@matthewfrost.com>
2012-09-01 18:25:35 -0400

    read from mongodb

--- src/dbhandler.cpp
+++ src/dbhandler.cpp
@@ -16,13 +16,21 @@ DBHandler::DBHandler() {
 	if (status != MONGO_OK) {
 		std::cerr << "Unable to connect to MONGODB\tError " << getMongoDBError(conn.err) << std::endl;	//DELME
 	}
-	bson b;
-	bson_init(&b);
-	bson_append_int(&b, "foo", 1811);
-	bson_append_long(&b, "bar", 236932520988528640);
-	bson_finish(&b);
-	bson_print( &b );
-	mongo_insert( &conn, "test.foo", &b, NULL );
+
+//	bson b;
+//	bson_init(&b);
+//	bson_append_int(&b, "foo", 1811);
+//	bson_append_long(&b, "bar", 236932520988528640);
+//	bson_finish(&b);
+//	bson_print( &b );
+//	mongo_insert( &conn, "test.foo", &b, NULL );
+
+	mongo_cursor cursor;
+	mongo_cursor_init (&cursor, &conn, "test.foo");
+	while (mongo_cursor_next(&cursor) == MONGO_OK) {
+		bson_print(&cursor.current);
+	}
+
 	mongo_destroy(&conn);
 }
 
