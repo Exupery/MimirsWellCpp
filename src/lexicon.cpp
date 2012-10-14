d528b160fbb1d0bf14deaf03d66292275610459f
Matthew Frost <mf@matthewfrost.com>
2012-10-14 10:28:02 -0400

    tokenize tweet

--- src/lexicon.cpp
+++ src/lexicon.cpp
@@ -4,7 +4,10 @@
  *  Created on: Oct 14, 2012
  *      Author: frost
  */
-
+#include <iostream>	//DELME
+#include <string>
+#include <sstream>
+//#include <vector>
 #include "lexicon.h"
 
 Lexicon::Lexicon() {
@@ -17,12 +20,28 @@ Lexicon::~Lexicon() {
 }
 
 void Lexicon::addWord(std::string& word) {
+	std::cout << word << std::endl;	//DELME
+	//TODO: ignore urls
+	//TODO: strip puncuation $ #
+	//TODO: set min length >= 2
+	//TODO: lowercase
+
 	words.insert(word);
 }
 
 int Lexicon::parseTweet(const std::string& tweet) {
 	int sizeBefore = words.size();
+//	std::cout << tweet << std::endl;	//DELME
+	std::string buffer;
+	std::stringstream ss(tweet);
 
+//	std::vector<std::string> tokens;
+
+	while (ss >> buffer) {
+//		tokens.push_back(buffer);
+//		std::cout << buffer << std::endl;	//DELME
+		addWord(buffer);
+	}
 
 	return words.size() - sizeBefore;
 }
